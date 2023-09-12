4939e97788c3dff018c70bdb90814da546da9395
Aleksandar Stojadinovic <astojadinovicsd@gmail.com>
2023-09-12 22:33:31 +0200

    added primeng, primeflex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './dashboard/dashboard.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, DashboardModule],
+  imports: [BrowserModule, BrowserAnimationsModule, DashboardModule],
   providers: [],
   bootstrap: [AppComponent],
 })
