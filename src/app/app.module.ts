aded5be3d3726ba586168ff5066c5d31849f9da8
Aleksandar Stojadinovic <astojadinovicsd@gmail.com>
2023-09-12 22:26:35 +0200

    added dashboard module and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, DashboardModule],
   providers: [],
   bootstrap: [AppComponent],
 })
